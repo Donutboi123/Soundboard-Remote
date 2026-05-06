2af594c115f26b547813971c5c53f7d27211941f
Logan Hersen <33905794+Donutboi123@users.noreply.github.com>
2026-05-06 18:58:10 -0400

    Modified program to work for multiple buttons.

--- program/program.ino
+++ program/program.ino
@@ -5,12 +5,19 @@
 SoftwareSerial mp3Serial(10, 11);
 DFRobotDFPlayerMini player;
 
-const int BTN = 2;
-bool last = HIGH;
+//Pins used for buttons
+const int buttonPins[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+
+//Previous button states
+bool last[8];
 
 void setup() {
 
-  pinMode(BTN, INPUT_PULLUP);
+  for (int i = 0; i < 8; i++) {
+
+    pinMode(buttonPins[i], INPUT_PULLUP);
+    last[i] = HIGH;
+  }
 
   Serial.begin(96000);
   mp3Serial.begin(9600);
@@ -26,12 +33,19 @@ void setup() {
 
 void loop() {
 
-  bool now = digitalRead(BTN);
+  for (int i = 0; i < 8; i++) {
 
-  if (last == HIGH && now == LOW) {
-    player.stop();
-    player.play(1);
+    bool now = digitalRead(buttonPins[i]);
+
+
+    //Detect button press
+    if (last[i] == HIGH && now == LOW) {
+      player.stop();
+      player.play(i + 1);
+
+      delay(200);
+    }
+
+    last[i] = now;
   }
-
-  last = now;
 }
